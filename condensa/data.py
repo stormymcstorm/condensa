507815426a43f91470e7169f6e58a4df7fee5dce
Carson Storm <stormymcstorm@gmail.com>
2021-04-06 16:50:26 -0600

    silence some errors

--- condensa/data.py
+++ condensa/data.py
@@ -27,7 +27,8 @@ def fast_collate(batch):
     h = imgs[0].size[1]
     tensor = torch.zeros((len(imgs), 3, h, w), dtype=torch.uint8)
     for i, img in enumerate(imgs):
-        nump_array = np.asarray(img, dtype=np.uint8)
+        nump_array = np.array(img, dtype=np.uint8)
+        # nump_array = np.asarray(img, dtype=np.uint8)
         if (nump_array.ndim < 3):
             nump_array = np.expand_dims(nump_array, axis=-1)
         nump_array = np.rollaxis(nump_array, 2)
